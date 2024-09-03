2e28de4d58d00147362c9eecdb0ec91706fde534
anawatson15 <142461052+anawatson15@users.noreply.github.com>
2024-09-03 19:26:08 -0300

    Add files via upload

---
+++ lab-olavo/src/animal.java
@@ -0,0 +1,8 @@
+
+public class animal {
+	
+	void FazerSom() {
+		System.out.println( "animal faz som");
+	}
+
+}
---
+++ lab-olavo/src/gato.java
@@ -0,0 +1,22 @@
+
+public class gato extends animal{
+	
+	public static void main(String[] args) {
+		
+		gato gato1 = new gato();
+		
+		gato1.FazerSom();
+		
+		
+		}
+	
+	
+	
+	void FazerSom(){
+		
+		System.out.println("O gato mia");
+	}
+	}
+	
+	
+
