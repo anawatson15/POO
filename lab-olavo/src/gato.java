7c47190d37e5e5a1b60d9cfb32b28894f82eff5f
anawatson15 <142461052+anawatson15@users.noreply.github.com>
2024-09-03 19:28:30 -0300

    Update gato.java

--- lab-olavo/src/gato.java
+++ lab-olavo/src/gato.java
@@ -4,19 +4,15 @@ public class gato extends animal{
 	public static void main(String[] args) {
 		
 		gato gato1 = new gato();
-		
 		gato1.FazerSom();
 		
-		
-		}
-	
-	
+	}
 	
 	void FazerSom(){
-		
-		System.out.println("O gato mia");
-	}
-	}
+	System.out.println("O gato mia");
+	
+}
+}
 	
 	
 
